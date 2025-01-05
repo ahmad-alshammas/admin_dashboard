882a8fedb554f354e963733b86c810f317933201
ahmad-alshamma <ahmad.a.alshammas@gmail.com>
2025-01-05 20:40:59 +0300

    Edit Links bootstrap , js

--- resources/views/user_side/inc/bootstrap.blade.php
+++ resources/views/user_side/inc/bootstrap.blade.php
@@ -3,15 +3,15 @@
  <!-- animate CSS -->
  <link rel="stylesheet" href="{{asset('user_side/css/animate.css')}}">
  <!-- owl carousel CSS -->
- <link rel="stylesheet" href="{{asset('user_side/css/owl.carousel.min.css')}}">
+ {{-- <link rel="stylesheet" href="{{asset('user_side/css/owl.carousel.min.css')}}"> --}}
  <!-- themify CSS -->
  <link rel="stylesheet" href="{{asset('user_side/css/themify-icons.css')}}">
  <!-- flaticon CSS -->
- <link rel="stylesheet" href="{{asset('user_side/css/flaticon.css')}}">
+ {{-- <link rel="stylesheet" href="{{asset('user_side/css/flaticon.css')}}"> --}}
  <!-- font awesome CSS -->
- <link rel="stylesheet" href="{{asset('user_side/css/magnific-popup.css')}}">
+ {{-- <link rel="stylesheet" href="{{asset('user_side/css/magnific-popup.css')}}"> --}}
  <!-- swiper CSS -->
- <link rel="stylesheet" href="{{asset('user_side/css/slick.css')}}">
+ {{-- <link rel="stylesheet" href="{{asset('user_side/css/slick.css')}}"> --}}
  <!-- style CSS -->
  <link rel="stylesheet" href="{{asset('user_side/css/style.css')}}">
 <!-- Font Awesome -->
